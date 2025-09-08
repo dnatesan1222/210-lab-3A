a0ded5dcc274cf294ab0d18d5fa83b9684db7111
Diksha <diksha@macbookpro>
2025-09-07 21:49:29 -0700

    test commit

---
+++ restaurant.cpp
@@ -0,0 +1,19 @@
+// COMSC-210 | Lab 3A | Diksha Tara Natesan
+// IDE used: VIM/Terminal
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Restaurant {
+    int maxTotal;       // maximum number of people the restaurant can serve
+    string name;        // restaurant name
+    string cuisine;     // restaurant's type of cuisine
+    double rating;      // restaurant's rating out of 5.0
+    string owner;       // owner's name
+};
+
+int main(){
+	cout << "test" << endl;
+}
