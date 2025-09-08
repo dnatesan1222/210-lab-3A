3b9a9af91023babb66303e9f6feb30ed4354cc88
Diksha <diksha@macbookpro>
2025-09-07 22:11:47 -0700

    commit final

--- restaurant.cpp
+++ restaurant.cpp
@@ -14,6 +14,56 @@ struct Restaurant {
     string owner;       // owner's name
 };
 
+
+//createRestaurant() takes user input to create a Restaurant object
+Restaurant createRestaurant() {
+    
+    Restaurant temp;
+
+    cout << "Restaurant name: ";
+    getline(cin, temp.name);
+
+    cout << "Cuisine type: ";
+    getline(cin, temp.cuisine);
+
+    cout << "Owner's name: ";
+    getline(cin, temp.owner);
+
+    cout << "Max customer occupancy: ";
+    cin >> temp.maxTotal;
+
+    cout << "Restaurant rating (1.0-5.0 star scale): ";
+    cin >> temp.rating;
+    cout << endl;
+
+    // returns: a Restaurant struct based upon the user's input
+    return temp;
+}
+
+
+// displayRestaurant() outputs a Restaurant object's data
+// arguments: takes in a restaurant object to use to output its information
+void displayRestaurant(Restaurant r) {
+    cout << "Restaurant Name: " << r.name << endl;
+    cout << "Cuisine Served: " << r.cuisine << endl;
+    cout << "Owner: " << r.owner << endl;
+    cout << "Max Occupancy: " << r.maxTotal << endl;
+    cout << "Rating: " << r.rating << " / 5.0\n" << endl;
+}
+
 int main(){
-	cout << "test" << endl;
+    Restaurant olive = {100, "Olive Garden", "Italian", 2.5, "Maria Zimmer"};
+    Restaurant imperial = {80, "Imperial Delight", "Chinese", 4.8, "Zoe Woon"};
+    Restaurant taco = {50, "Taco Bell", "Mexican", 3.1, "Sam Martin"};
+
+    // restaurant from user input
+    cout << "Create a new restaurant entry:\n";
+    Restaurant userInput = createRestaurant();
+
+    // display all restaurants
+    displayRestaurant(olive);
+    displayRestaurant(imperial);
+    displayRestaurant(taco);
+    displayRestaurant(userInput);
+
 }
